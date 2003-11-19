ebf103aabec13af4f37e3afc89479e28ecdd162e
elharo <elharo@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2003-11-19 20:19:41 +0000

    Removing imports that are no longer needed

--- trunk/src/nu/xom/tests/CanonicalizerTest.java
+++ trunk/src/nu/xom/tests/CanonicalizerTest.java
@@ -28,11 +28,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import com.ibm.icu.text.Normalizer;
 
