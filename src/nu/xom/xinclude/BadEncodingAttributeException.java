8d424268ab24694569b90e6f98f651cdc101b48a
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2004-10-24 13:34:54 +0000

    Treating bad encoding attributes as fatal errors. This is tentative, and depends on the eventual decision of the XInclude working group.

---
+++ src/nu/xom/xinclude/BadEncodingAttributeException.java
@@ -0,0 +1,65 @@
+/* Copyright 2004 Elliotte Rusty Harold
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 2.1 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the 
+   Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+   Boston, MA 02111-1307  USA
+   
+   You can contact Elliotte Rusty Harold by sending e-mail to
+   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   subject line. The XOM home page is temporarily located at
+   http://www.cafeconleche.org/XOM/  but will eventually move
+   to http://www.xom.nu/  */
+
+package nu.xom.xinclude;
+
+/**
+ * <p>
+ * Indicates that the <code>parse</code> attribute has some value other 
+ * than <code>xml</code> or <code>text</code>.
+ * </p>
+ *
+ * @author Elliotte Rusty Harold
+ * @version 1.0b7
+ */
+public class BadEncodingAttributeException extends XIncludeException {
+
+    
+    /**
+     * <p>
+     * Constructs a <code>BadParseAttributeException</code> with 
+     * the specified detail message. 
+     * </p>
+     *
+     * @param message a string indicating the specific problem
+     */
+    public BadEncodingAttributeException(String message) {
+        super(message);
+    }
+
+    
+    /**
+     * <p>
+     * Creates a new <code>BadParseAttributeException</code> with a  
+     * detail message and the URI of the document that caused 
+     * the exception.
+     * </p>
+     * 
+     * @param message a string indicating the specific problem
+     * @param uri the URI of the document that caused this exception
+     */
+    public BadEncodingAttributeException(String message, String uri) {
+        super(message, uri);
+    }
+
+    
+}
