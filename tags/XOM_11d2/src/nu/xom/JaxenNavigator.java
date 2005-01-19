1378f3a351145af874a2c81dbf1438064340e232
nobody <nobody@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2005-01-19 16:30:25 +0000

    This commit was manufactured by cvs2svn to create tag 'XOM_11d2'.

---
+++ tags/XOM_11d2/src/nu/xom/JaxenNavigator.java
@@ -0,0 +1,495 @@
+/* Copyright 2005 Elliotte Rusty Harold
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 2.1 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the 
+   Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+   Boston, MA 02111-1307  USA
+   
+   You can contact Elliotte Rusty Harold by sending e-mail to
+   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   subject line. The XOM home page is located at http://www.xom.nu/
+*/
+
+package nu.xom;
+
+
+import nu.xom.Attribute;
+import nu.xom.Comment;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.ProcessingInstruction;
+import nu.xom.Text;
+import nu.xom.Node;
+import nu.xom.ParentNode;
+
+import org.jaxen.DefaultNavigator;
+import org.jaxen.FunctionCallException;
+import org.jaxen.JaxenException;
+import org.jaxen.XPath;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+class JaxenNavigator extends DefaultNavigator {
+
+    
+    public Iterator getSelfAxisIterator(Object contextNode) {
+        
+        if (contextNode instanceof Text) {
+            // wrap text nodes in a list
+            Text node = (Text) contextNode;
+            ArrayList temp = new ArrayList();
+            ParentNode parent = node.getParent();
+            // parent is never null here due to DocumentFragment
+            int index = parent.indexOf(node);
+            int first = index;
+            int last = index;
+            while (first > 0 && parent.getChild(first-1).isText()) {
+                first--;
+            }
+            while (last < parent.getChildCount()-1 && parent.getChild(last+1).isText()) {
+                last++;
+            }
+            for (int i = first; i <= last; i++) {
+                temp.add(parent.getChild(i));
+            }
+            contextNode = temp;
+        }
+        List l = new ArrayList(1);
+        l.add(contextNode);
+        return l.iterator();
+    }
+    
+    
+    public Object getElementById(Object node, String id) {
+        
+        Node original = (Node) node;
+        ParentNode parent;
+        if (original.isElement() || original.isDocument()) {
+            parent = (ParentNode) original;
+        }
+        else {
+            parent = original.getParent();
+        }
+        
+        // find highest parent node
+        ParentNode high = parent;
+        while (parent != null) {
+            high = parent;
+            parent = parent.getParent();
+        }
+        
+        // Now search down from the highest point for the requested ID
+        Element root;
+        if (high.isDocument()) {
+            root = ((Document) high).getRootElement();
+        }
+        else { // document fragment
+            Node first = high.getChild(0);
+            if (first.isElement()) {
+                root = (Element) high.getChild(0);
+            }
+            else {
+                return null;
+            }
+        }
+        
+        return findByID(root, id);
+        
+    }
+    
+    
+    // ????remove recursion
+    public static Element findByID(Element top, String id) {
+        
+        if (hasID(top, id)) return top;
+        else {
+            Elements children = top.getChildElements();
+            for (int i = 0; i < children.size(); i++) {
+                Element result = findByID(children.get(i), id);
+                if (result != null) return result;
+            }
+        }
+        return null;
+        
+    }
+    
+    
+    private static boolean hasID(Element top, String id) {
+
+        for (int i = 0; i < top.getAttributeCount(); i++) {
+            Attribute a = top.getAttribute(i);
+            if (Attribute.Type.ID == a.getType()) {
+                // ???? really need to fully normalize here
+                return a.getValue().trim().equals(id);
+            }
+        }
+        return false;
+    }
+
+
+    public String getNamespacePrefix(Object o) {
+        XPathNamespaceNode ns = (XPathNamespaceNode) o;
+        return ns.prefix;
+    }
+    
+    
+    public String getNamespaceStringValue(Object o) {
+        XPathNamespaceNode ns = (XPathNamespaceNode) o;
+        return ns.uri;
+    }
+
+    
+    static class XPathNamespaceNode {
+        
+        XPathNamespaceNode(String prefix, String uri) {
+            this.prefix = prefix;
+            this.uri = uri;
+        }
+        
+        String prefix;
+        String uri;
+        
+    }
+    
+    
+    public Iterator getNamespaceAxisIterator(Object contextNode) {
+        
+        try {
+            Element element = (Element) contextNode;
+            // ???? can probably avoid this list copy
+            Map bindings = element.getNamespacePrefixesInScope();
+            Iterator iterator = bindings.keySet().iterator();
+            List result = new ArrayList(bindings.size()+1);
+            result.add(new XPathNamespaceNode("xml", "http://www.w3.org/XML/1998/namespace"));
+
+            while (iterator.hasNext()) {
+                String prefix = (String) iterator.next();
+                String uri = (String) bindings.get(prefix);
+                if (! "".equals(prefix) || ! "".equals(uri)) {
+                    XPathNamespaceNode ns = new XPathNamespaceNode(prefix, uri);
+                    result.add(ns);
+                }
+            }
+            return result.iterator();
+        }
+        catch (ClassCastException ex) {
+            return Collections.EMPTY_LIST.iterator();
+        }
+    }
+    
+    
+    public Iterator getParentAxisIterator(Object contextNode)  {
+        
+        Node n = (Node) contextNode;
+        Node parent = n.getParent();
+        if (parent == null) return Collections.EMPTY_LIST.iterator();
+        else {
+            List l = new ArrayList(1);
+            l.add(parent);
+            return l.iterator();
+        }
+        
+    }
+    
+    
+    public Object getDocumentNode(Object o) {
+    
+        Node node = (Node) o;
+        return node.getRoot();
+        
+    }
+    
+        
+    public Object getDocument(String url) throws FunctionCallException {
+        throw new FunctionCallException("document() function not supported");
+    }
+    
+    public Iterator getAttributeAxisIterator(Object contextNode) {
+        
+        try {
+            Element element = (Element) contextNode;
+            if (element.attributes == null) {
+                return Collections.EMPTY_LIST.iterator();
+            }
+            else return element.attributes.iterator();
+        }
+        catch (ClassCastException ex) {
+            return Collections.EMPTY_LIST.iterator();
+        }
+        
+    }
+    
+    
+    public Iterator getChildAxisIterator(Object o) {
+        
+        if (o instanceof ParentNode) {
+            return new ChildIterator((ParentNode) o);
+        }
+        else {
+            return Collections.EMPTY_LIST.iterator();
+        }
+        
+    }
+    
+    
+    public Object getParentNode(Object o) {
+        Node n = (Node) o;
+        return n.getParent();
+    }
+
+    
+    private static int getXPathChildCount(ParentNode parent) {
+    
+        if (parent instanceof Document) {
+            DocType doctype = ((Document) parent).getDocType();
+            if (doctype == null) return parent.getChildCount();
+            else return parent.getChildCount() - 1;
+        }
+        int children = 0;
+        
+        boolean previousWasText = false;
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            Node child = parent.getChild(i);
+            if (child instanceof Text && !previousWasText) {
+                if (child.getValue().length() != 0) {
+                    children++;
+                    previousWasText = true;
+                }
+            }
+            else {
+                previousWasText = false;
+                children++;
+            }
+        }
+        return children;
+        
+    }
+    
+    public String getTextStringValue(Object o) {
+        
+        List texts = (List) o;
+        String result = "";
+        Iterator iterator = texts.iterator();
+        while (iterator.hasNext()) {
+            Text text = (Text) iterator.next();
+            result += text.getValue();
+        }
+        return result;
+        
+    }
+    
+    
+    // Need to make sure we don't count DocType in children when 
+    // working with XPath
+    private static Object getXPathChild(int request, Document parent) {
+     
+        DocType doctype = parent.getDocType();
+        if (doctype == null) return parent.getChild(request);
+        else {
+            int doctypePosition = parent.indexOf(doctype);
+            if (request < doctypePosition) {
+                return parent.getChild(request);
+            }
+            else return parent.getChild(request+1);
+        }
+        
+    }
+    
+
+    private static Object getXPathChild(int request, ParentNode parent) {
+    
+        if (parent instanceof Document) {
+            return getXPathChild(request, (Document) parent);
+        }
+        
+        int childCount = 0;
+        
+        boolean previousWasText = false;
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            Node child = parent.getChild(i);
+            
+            // check to see if previous was text and if so go to next
+            if (child instanceof Text && previousWasText) continue;
+            
+            if (request == childCount) {
+                if (child instanceof Text) {
+                    StringBuffer sb = new StringBuffer();
+                    List list = new ArrayList();
+                    int textCount = i;
+                    do {
+                        Text temp = (Text) child;
+                        list.add(temp);
+                        sb.append(child.getValue());
+                        textCount++;
+                        if (textCount == parent.getChildCount()) break;
+                        child = parent.getChild(textCount);
+                    } while (child instanceof Text);
+                     
+                    
+                    if (sb.length() != 0) return list;
+                }
+                else {
+                    return child;
+                }
+            }
+            else {
+                if (child instanceof Text && !previousWasText) {
+                    if (child.getValue().length() != 0) {
+                      childCount++;
+                      previousWasText = true;
+                    }
+                }
+                else {
+                    previousWasText = false;
+                    childCount++;
+                }
+            }
+        }
+        
+        return null; 
+        
+    }
+    
+
+    private static class ChildIterator implements Iterator {
+    
+        private ParentNode parent;
+
+        private int index = 0;
+        private int end;
+        
+        ChildIterator(ParentNode parent) {
+            this.parent = parent;
+            this.end = getXPathChildCount(parent);
+        }
+        
+        public boolean hasNext() {
+            return index < end;
+        }
+        
+        public Object next() {
+            return getXPathChild(index++, parent);
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        
+    }
+    
+
+    public String getElementNamespaceUri(Object element) {
+        return ((Element) element).getNamespaceURI();
+    }
+
+    
+    // In Jaxen, name means the local name only 
+    public String getElementName(Object element) {
+        return ((Element) element).getLocalName();
+    }
+
+    public String getElementQName(Object element) {
+        return ((Element) element).getQualifiedName();
+    }
+
+    
+    public String getAttributeNamespaceUri(Object attr) {
+        Attribute attribute = (Attribute) attr;
+        return attribute.getNamespaceURI();
+    }
+
+    
+    // In Jaxen, name means the local name only 
+    public String getAttributeName(Object attr) {
+        Attribute attribute = (Attribute) attr;
+        return attribute.getLocalName();
+    }
+
+    
+    public String getAttributeQName(Object attr) {
+        Attribute attribute = (Attribute) attr;
+        return attribute.getQualifiedName();
+    }
+
+    public String getProcessingInstructionData(Object o) {
+        ProcessingInstruction pi = (ProcessingInstruction) o;
+        return pi.getValue();
+    }
+
+   
+    public String getProcessingInstructionTarget(Object o) {
+        ProcessingInstruction pi = (ProcessingInstruction) o;
+        return pi.getTarget();
+    }
+
+    
+    public boolean isDocument(Object object) {
+        return object instanceof Document || object instanceof DocumentFragment;
+    }
+
+    
+    public boolean isElement(Object object) {
+        return object instanceof Element;
+    }
+
+    
+    public boolean isAttribute(Object object) {
+        return object instanceof Attribute;
+    }
+
+    
+    public boolean isNamespace(Object object) {
+        return object instanceof XPathNamespaceNode;
+    }
+
+    
+    public boolean isComment(Object object) {
+        return object instanceof Comment;
+    }
+
+    
+    public boolean isText(Object object) {
+        return object instanceof ArrayList;
+    }
+
+    
+    public boolean isProcessingInstruction(Object object) {
+        return object instanceof ProcessingInstruction;
+    }
+
+    
+    public String getCommentStringValue(Object comment) {
+        return ((Comment) comment).getValue();
+    }
+
+    
+    public String getElementStringValue(Object element) {
+        return ((Element) element).getValue();
+    }
+
+    
+    public String getAttributeStringValue(Object attribute) {
+        return ((Attribute) attribute).getValue();
+    }
+    
+
+    public XPath parseXPath(String expression) throws JaxenException {
+        return new JaxenConnector(expression);
+    }
+    
+    
+}
---
+++ tags/XOM_11d2/src/nu/xom/tests/CommentTest.java
@@ -0,0 +1,322 @@
+/* Copyright 2002-2004 Elliotte Rusty Harold
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 2.1 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the 
+   Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+   Boston, MA 02111-1307  USA
+   
+   You can contact Elliotte Rusty Harold by sending e-mail to
+   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   subject line. The XOM home page is located at http://www.xom.nu/
+*/
+
+package nu.xom.tests;
+
+import nu.xom.Comment;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.IllegalDataException;
+import nu.xom.IllegalCharacterDataException;
+
+
+/**
+ * <p>
+ *  Various basic unit tests for the <code>Comment</code> class.
+ * </p>
+ * 
+ * @author Elliotte Rusty Harold
+ * @version 1.0
+ *
+ */
+public class CommentTest extends XOMTestCase {
+
+    
+    public CommentTest(String name) {
+        super(name);
+    }
+
+    
+    public void testConstructor() {
+        Comment c1 = new Comment("test");
+        assertEquals("test", c1.getValue());
+        Comment c2 = new Comment("");
+        assertEquals("", c2.getValue());
+    }
+
+    
+    public void testCopyConstructor() {
+        Comment c1 = new Comment("test");
+        Comment c2 = new Comment(c1);
+        assertEquals("test", c2.getValue());
+        assertEquals(c1.getValue(), c2.getValue());
+        assertTrue(c1 != c2);     
+    }
+
+    
+    public void testToString() {
+        
+        Comment c1 = new Comment("content");
+        assertEquals("[nu.xom.Comment: content]", c1.toString());          
+        
+        c1.setValue("012345678901234567890123456789012345678901234567890123456789");
+        assertEquals(
+          "[nu.xom.Comment: 01234567890123456789012345678901234...]", 
+          c1.toString()
+        ); 
+           
+    }
+    
+    
+    public void testToStringWithLineFeed() {
+        
+        Comment c = new Comment("content\ncontent");
+        assertEquals("[nu.xom.Comment: content\\ncontent]", c.toString());         
+        
+    }
+    
+    
+    public void testToStringWithLotsOfData() {
+        
+        Comment c 
+          = new Comment("content content 012345678901234567890123456789012345678901234567890123456789");
+        String s = c.toString();
+        assertTrue(s.length() < 72);
+        
+    }
+
+
+    public void testToXML() {
+        
+        Comment c1 = new Comment("content");
+        assertEquals("<!--content-->", c1.toXML());          
+        
+        c1.setValue(" 012345678901234567890123456789012345678901234567890123456789 ");
+        assertEquals(
+          "<!-- 012345678901234567890123456789012345678901234567890123456789 -->", 
+          c1.toXML()
+        ); 
+           
+    }
+    
+    
+    // This is a problem because it cannot be serialized
+    // since character and entity references aren't
+    // recognized in comment data
+    public void testCarriageReturnInCommentData() {
+        try {
+            new Comment("data\rdata");
+            fail("Allowed carriage return in comment");
+        }
+        catch (IllegalDataException success) {
+            assertEquals("data\rdata", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+    }
+    
+    
+    public void testSetter() {
+
+        Comment c1 = new Comment("test");
+        c1.setValue("legal");
+        assertEquals("legal", c1.getValue());
+        
+        try {
+          c1.setValue("test -- test");
+          fail("Should raise an IllegalDataException");
+        }
+        catch (IllegalDataException success) {
+            assertEquals("test -- test", success.getData());
+            assertNotNull(success.getMessage());   
+        }
+        
+        try {
+          c1.setValue("test-");
+          fail("Should raise an IllegalDataException");
+        }
+        catch (IllegalDataException success) {
+            assertEquals("test-", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+        c1.setValue(null);
+        assertEquals("", c1.getValue());
+
+     }
+
+
+    public void testEquals() {
+        
+        Comment c1 = new Comment("test");
+        Comment c2 = new Comment("test");
+        Comment c3 = new Comment("skjlchsakdjh");
+
+        assertEquals(c1, c1);
+        assertEquals(c1.hashCode(), c1.hashCode());
+        assertTrue(!c1.equals(c2));
+        assertTrue(!c1.equals(c3));
+        
+    }
+
+    
+    public void testCopy() {
+        
+        Comment c1 = new Comment("test");
+        Comment c2 = (Comment) c1.copy();
+
+        assertEquals(c1.getValue(), c2.getValue());
+        assertTrue(!c1.equals(c2));
+        assertNull(c2.getParent());
+
+    }
+
+    
+    // Check passing in a string with broken surrogate pairs
+    // and with correct surrogate pairs
+    public void testSurrogates() {
+
+        String goodString = "test: \uD8F5\uDF80  ";
+        Comment c = new Comment(goodString);
+        assertEquals(goodString, c.getValue());
+
+        // Two high-halves
+        try {
+          new Comment("test: \uD8F5\uDBF0  ");
+          fail("Should raise an IllegalCharacterDataException");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals("test: \uD8F5\uDBF0  ", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+        // Two high-halves
+        try {
+          new Comment("test: \uD8F5\uD8F5  ");
+          fail("Should raise an IllegalCharacterDataException");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals("test: \uD8F5\uD8F5  ", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+        // One high-half
+        try {
+           new Comment("test: \uD8F5  ");
+           fail("Should raise an IllegalCharacterDataException");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals("test: \uD8F5  ", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+        // One low half
+        try {
+            new Comment("test: \uDF80  ");
+            fail("Should raise an IllegalCharacterDataException");
+         }
+        catch (IllegalCharacterDataException success) {
+            assertEquals("test: \uDF80  ", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+        // Low half before high half
+        try {
+            new Comment("test: \uDCF5\uD8F5  ");
+            fail("Should raise an IllegalCharacterDataException");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals("test: \uDCF5\uD8F5  ", success.getData());
+            assertNotNull(success.getMessage());   
+        }   
+
+
+    }
+
+    
+    public void testLeafNode() {
+
+        Comment c1 = new Comment("data");
+        assertEquals(0, c1.getChildCount());
+        try {
+            c1.getChild(0);
+            fail("Didn't throw IndexOutofBoundsException");
+        }
+        catch (IndexOutOfBoundsException success) {
+            assertNotNull(success.getMessage()); 
+        }
+        
+        assertNull(c1.getParent());
+
+        Element element = new Element("test");
+        element.appendChild(c1);
+        assertEquals(element, c1.getParent());
+        assertEquals(element.getChild(0), c1);
+
+        element.removeChild(c1);
+        assertTrue(element.getChildCount() == 0);
+
+    }
+
+    
+    public void testGetDocument() {
+
+        Comment c1 = new Comment("data");
+        assertNull(c1.getDocument());
+        Element root = new Element("root");
+        root.appendChild(c1);
+        assertNull(c1.getDocument());
+        Document doc = new Document(root);
+        assertEquals(doc, c1.getDocument());
+
+    }
+
+    
+    public void testAllowReservedCharactersInData() {
+        Comment comment = new Comment("<test>&amp;&greater;");
+        String xml = comment.toXML();
+        assertEquals("<!--<test>&amp;&greater;-->", xml);  
+    }
+
+
+    public void testForbidIllegalCharactersInComments() {
+        
+        try {
+            new Comment(" \u0001 ");
+            fail("Allowed C0 control in comment");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals(" \u0001 ", success.getData());
+            assertNotNull(success.getMessage());
+        }
+          
+    }
+
+
+    public void testForbidUnmatchedSurrogatesInComments() {
+        
+        try {
+            new Comment(" \uD800 ");
+            fail("Allowed unmatched high surrogate in comment");
+        }
+        catch (IllegalCharacterDataException success) {
+            assertEquals(" \uD800 ", success.getData());
+            assertNotNull(success.getMessage());
+        }
+          
+    }
+
+    
+    public void testCommentDataCanStartWithAHyphen() {
+        Comment c = new Comment("- - ");
+        assertEquals("- - ", c.getValue());
+    }
+
+}
