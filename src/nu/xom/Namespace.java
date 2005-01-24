4bf930844f7b80184adedec1d3d2a7910e3c00db
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-01-24 23:39:53 +0000

    Adding JavaDoc

--- src/nu/xom/Namespace.java
+++ src/nu/xom/Namespace.java
@@ -25,7 +25,7 @@ import nu.xom.Element;
 
 /**
  * <p>
- * This class represents a namespace in scope. It is used by XOM's 
+ * Represents a namespace in scope. It is used by XOM's 
  * XPath implementation for the namespace axis. However, it is not 
  * really part of the XOM data model. Namespace objects are only
  * created as needed when evaluating XPath.
@@ -41,10 +41,14 @@ public final class Namespace extends Node {
     private String uri;
     private Element parent;
     
+    /**
+     * Namespace URI specified for <code>xml</code> prefix
+     */
     public final static String XML_NAMESPACE 
       = "http://www.w3.org/XML/1998/namespace";
 
-    public Namespace(String prefix, String uri, Element parent) {
+    
+    Namespace(String prefix, String uri, Element parent) {
         this.prefix = prefix;
         this.uri = uri;
         super.setParent(parent);
@@ -52,6 +56,11 @@ public final class Namespace extends Node {
     
     
     /**
+     * <p>
+     * Returns the namespace prefix, or the empty string if this node
+     * is the default namespace.
+     * </p>
+     * 
      * @return the namespace prefix
      */
     public String getPrefix() {
@@ -59,33 +68,92 @@ public final class Namespace extends Node {
     }
 
 
+    /**
+     * <p>
+     * Returns the namespace URI.
+     * </p>
+     * 
+     * @return the namespace URI
+     */
     public String getValue() {
         return uri;
     }
 
 
+     /**
+     * <p>
+     * Throws <code>IndexOutOfBoundsException</code> because 
+     * namespaces do not have children.
+     * </p>
+     * 
+     * @return never returns because document type declarations do not 
+     *     have children. Always throws an exception.
+     * 
+     * @param position the index of the child node to return
+     * 
+     * @throws IndexOutOfBoundsException because document type declarations
+     *     do not have children
+     */
     public Node getChild(int position) {
         throw new IndexOutOfBoundsException(
           "Namespaces do not have children");
     }
 
 
+    /**
+     * <p>
+     * Returns 0 because namespaces do not have 
+     * children.
+     * </p>
+     * 
+     * @return zero
+     */
     public int getChildCount() {
         return 0;
     }
 
 
+    /**
+     * <p>
+     *   Returns a copy of this namespace which has 
+     *   the same prefix and URI, but no parent.
+     * </p>
+     * 
+     * @return a deep copy of this <code>Namespace</code> 
+     *     that is not part of a document
+     */
     public Node copy() {
         return new Namespace(prefix, uri, null);
     }
 
 
+    /**
+     * <p>
+     *  Returns a string containing the actual XML
+     *  form of the namespace declaration represented
+     *  by this object. For example, 
+     *  <code>xmlns:pre="http://www.example.org/"</code>. 
+     * </p>
+     * 
+     * @return a <code>String</code> containing 
+     *      an XML namespace declaration
+     */
     public String toXML() {
         String colon = prefix.equals("") ? "" : ":";
         return "xmlns" + colon + prefix + "=\"" + uri + "\"";
     }
     
     
+    /**
+     * <p>
+     * Returns a string form of the 
+     * <code>Namespace</code> suitable for debugging
+     * and diagnosis. It deliberately does not return 
+     * an actual XML namespace declaration. 
+     * </p>
+     * 
+     * @return a string representation of this object
+     */
     public String toString() {
         return "[Namespace: " + this.toXML() + "]";
     }
