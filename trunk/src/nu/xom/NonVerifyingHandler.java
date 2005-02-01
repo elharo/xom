8f30f64df3db11acc138f63c8dba010dd0bdc938
elharo <elharo@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2005-02-01 14:05:24 +0000

    Removing unused local variable

--- trunk/src/nu/xom/NonVerifyingHandler.java
+++ trunk/src/nu/xom/NonVerifyingHandler.java
@@ -1,4 +1,4 @@
-/* Copyright 2002-2004 Elliotte Rusty Harold
+/* Copyright 2002-2005 Elliotte Rusty Harold
    
    This library is free software; you can redistribute it and/or modify
    it under the terms of version 2.1 of the GNU Lesser General Public 
@@ -24,7 +24,7 @@ package nu.xom;
 
 /**
  * @author Elliotte Rusty Harold
- * @version 1.0
+ * @version 1.1d5
  *
  */
 class NonVerifyingHandler extends XOMHandler {
@@ -134,9 +134,7 @@ class NonVerifyingHandler extends XOMHandler {
         
         if (parent.isDocument()) { // root element
             Document doc = (Document) parent;
-            boolean beforeRoot = true;
             doc.setRootElement((Element) current);
-            beforeRoot = false;
         }
         
     }
