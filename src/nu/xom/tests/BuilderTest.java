74c94813880e5833445eb0055e983dff1759d456
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-02-20 00:49:56 +0000

    Speeding up processTests which was taking roughly 5% of the entire test suite execution time

--- src/nu/xom/tests/BuilderTest.java
+++ src/nu/xom/tests/BuilderTest.java
@@ -75,7 +75,7 @@ import nu.xom.XMLException;
  * </p>
  * 
  * @author Elliotte Rusty Harold
- * @version 1.1d4
+ * @version 1.1d7
  *
  */
 public class BuilderTest extends XOMTestCase {
@@ -2965,7 +2965,12 @@ public class BuilderTest extends XOMTestCase {
     private void processTests(Elements tests) 
       throws ParsingException, IOException  {
         
-        for (int i = 0; i < tests.size(); i++) {
+        Element parent = new Element("e");
+        Element child = new Element("a");
+        parent.appendChild(child);
+            
+        int size = tests.size();
+        for (int i = 0; i < size; i++) {
             Element test = tests.get(i);
             String namespace = test.getAttributeValue("NAMESPACE");
             if ("no".equals(namespace)) continue;
@@ -2975,12 +2980,9 @@ public class BuilderTest extends XOMTestCase {
             String base = test.getBaseURI();
             // Hack because URIUtil isn't public; and I don't want to
             // depend on 1.4 only java.net.URI
-            Element parent = new Element("e");
             parent.setBaseURI(base);
-            Element child = new Element("a");
             child.addAttribute(new Attribute("xml:base", 
               "http://www.w3.org/XML/1998/namespace", uri));
-            parent.appendChild(child);
             String resolvedURI = child.getBaseURI();
             
             Document doc = builder.build(resolvedURI);
