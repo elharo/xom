ea2cc16f9d3dc34a38d07fdff38c3383b4d029ee
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2005-12-27 18:15:20 +0000

    Adding license

--- src/nu/xom/samples/inner/InnerTest.java
+++ src/nu/xom/samples/inner/InnerTest.java
@@ -1,3 +1,24 @@
+/* Copyright 2005 Elliotte Rusty Harold
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 2.1 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the 
+   Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+   Boston, MA 02111-1307  USA
+   
+   You can contact Elliotte Rusty Harold by sending e-mail to
+   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   subject line. The XOM home page is located at http://www.xom.nu/
+*/
+
 package nu.xom.samples.inner;
 
 import java.io.IOException;
@@ -9,9 +30,6 @@ import nu.xom.*;
 public class InnerTest extends TestCase {
 
     private Builder builder = new Builder(new InnerFactory());
-    
-    protected void setUp() {
-    }
     
     public void testGetInner() throws ValidityException, ParsingException, IOException {
         Document doc = builder.build("<root><a>test</a><b>test2</b></root>", null);
