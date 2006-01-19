0552f7ef96ba48956eb75597c0716a7708e8f444
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2006-01-19 20:10:12 +0000

    Moving some xml:space logic into writeTagBeginning, writeStartTag, and writeEndTag to enable streaming serializers

--- src/nu/xom/tests/SerializerTest.java
+++ src/nu/xom/tests/SerializerTest.java
@@ -1526,7 +1526,7 @@ public class SerializerTest extends XOMTestCase {
         );
         
     }
-
+    
     
     public void testIndentingFromSubclassThatAvoidsWriteElement() 
       throws IOException {
@@ -1544,6 +1544,30 @@ public class SerializerTest extends XOMTestCase {
         assertEquals(
           "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
           + "<itemSet>\r\n    <item1></item1>\r\n    <item2></item2>\r\n"
+          + "</itemSet>\r\n", 
+          result
+        );
+        
+    }
+    
+    
+    public void testIndentingFromSubclassThatAvoidsWriteElementWithXMLSpace() 
+      throws IOException {
+        
+        Element items = new Element("itemSet");
+        items.addAttribute(new Attribute("xml:space", Namespace.XML_NAMESPACE, "preserve"));
+        items.appendChild(new Element("item1"));
+        items.appendChild(new Element("item2"));
+        Document doc = new Document(items);
+        Serializer serializer = new AvoidWriteElement(out);
+        serializer.setIndent(4);
+        serializer.write(doc);
+        serializer.flush();
+        out.close();
+        String result = new String(out.toByteArray(), "UTF-8");
+        assertEquals(
+          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+          + "<itemSet xml:space=\"preserve\"><item1></item1><item2></item2>"
           + "</itemSet>\r\n", 
           result
         );
