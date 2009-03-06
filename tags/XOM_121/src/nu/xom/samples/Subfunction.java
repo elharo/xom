27cabe58d7c4e57d5c22d5baa69054f13e7beff8
nobody <nobody@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2009-03-06 14:01:36 +0000

    This commit was manufactured by cvs2svn to create tag 'XOM_121'.

---
+++ tags/XOM_121/src/nu/xom/samples/Subfunction.java
@@ -0,0 +1,69 @@
+/* Copyright 2002, 2003 Elliotte Rusty Harold
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 2.1 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the 
+   Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+   Boston, MA 02111-1307  USA
+   
+   You can contact Elliotte Rusty Harold by sending e-mail to
+   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   subject line. The XOM home page is located at http://www.xom.nu/
+*/
+
+package nu.xom.samples;
+
+import nu.xom.Element;
+
+
+/**
+ * 
+ * <p>
+ * Demonstrates building a structured XML document,
+ * from flat, tabular data. A different version of this 
+ * example was originally developed for Chapter 4 of 
+ * <cite><a  target="_top"
+ * href="http://www.cafeconleche.org/books/xmljava/">Processing 
+ * XML with Java</a></cite>.
+ * </p>
+ * 
+ * @author Elliotte Rusty Harold
+ * @version 1.0
+ *
+ */
+public class Subfunction {
+ 
+  private String code;
+  private String title;
+  private long   amount;
+    
+  public Subfunction(String title, String code, long amount) {
+        
+    this.title  = title;
+    this.code   = code;
+    this.amount = amount;
+    
+  }
+  
+  public Element getXML() {
+        
+    Element subfunction = new Element("Subfunction");
+    Element name = new Element("Name");
+    Element code = new Element("Code");
+    Element amount = new Element("Amount");
+    name.appendChild(this.title);
+    code.appendChild(this.code);
+    amount.appendChild(String.valueOf(this.amount));
+    return subfunction;
+    
+  } 
+               
+}
