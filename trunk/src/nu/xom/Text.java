7543ed35993d4d7369ccce372e66b776942a7b53
elharo <elharo@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2010-11-22 10:52:42 +0000

    Javadoc info about really big text nodes.

--- trunk/src/nu/xom/Text.java
+++ trunk/src/nu/xom/Text.java
@@ -1,4 +1,4 @@
-/* Copyright 2002-2005 Elliotte Rusty Harold
+/* Copyright 2002-2005, 2010 Elliotte Rusty Harold
    
    This library is free software; you can redistribute it and/or modify
    it under the terms of version 2.1 of the GNU Lesser General Public 
@@ -15,7 +15,7 @@
    Boston, MA 02111-1307  USA
    
    You can contact Elliotte Rusty Harold by sending e-mail to
-   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   elharo@ibiblio.org. Please include the word "XOM" in the
    subject line. The XOM home page is located at http://www.xom.nu/
 */
 
@@ -30,9 +30,17 @@ import java.io.UnsupportedEncodingException;
  *   normal text. <code>Text</code> objects may be adjacent to other 
  *   <code>Text</code> objects.
  * </p>
-
+ * 
+ * <p>
+ *   The maximum size of a String or an array in Java limits the maximum
+ *   size of a text node to about 2 gigabytes. If you're stuffing Base-64 
+ *   encoded movies inside one node, you will have problems. Try breaking 
+ *   up the text into smaller, contiguous chunks. Even then you will
+ *   probably not be able to use XPath on the result. 
+ * </p>
+ *
  * @author Elliotte Rusty Harold
- * @version 1.1b3
+ * @version 1.2.7
  *
  */
 public class Text extends Node {
