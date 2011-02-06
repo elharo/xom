43363a05cd5c48d4914e16339d7469dfd173f770
elharo <elharo@60df1150-f05e-c20b-e263-d8ae73ba91d8>
2011-02-06 14:44:33 +0000

    Some tests have started failing due to JDK changes with respect to handling spaces in paths. This cl fixes that, though the tests now depend on Java 1.4 or later. Hopefully in 2011 this is not an issue for anyone.

--- trunk/src/nu/xom/tests/DocumentTest.java
+++ trunk/src/nu/xom/tests/DocumentTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2002-2004 Elliotte Rusty Harold
+/* Copyright 2002-2004, 2011 Elliotte Rusty Harold
    
    This library is free software; you can redistribute it and/or modify
    it under the terms of version 2.1 of the GNU Lesser General Public 
@@ -15,7 +15,7 @@
    Boston, MA 02111-1307  USA
    
    You can contact Elliotte Rusty Harold by sending e-mail to
-   elharo@metalab.unc.edu. Please include the word "XOM" in the
+   elharo@ibiblio.org. Please include the word "XOM" in the
    subject line. The XOM home page is located at http://www.xom.nu/
 */
 
@@ -576,7 +576,7 @@ public class DocumentTest extends XOMTestCase {
         f = new File(f, "test.xml");   
         Document input = builder.build(f);
         String s = input.toXML();
-        Document output = builder.build(s, f.toURL().toExternalForm());
+        Document output = builder.build(s, f.toURI().toString());
         assertEquals(input, output);
         
     }
