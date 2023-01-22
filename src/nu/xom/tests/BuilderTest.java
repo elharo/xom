555804f238d391facdfbcf0b0ed2fd9f60713303
Elliotte Rusty Harold <elharo@ibiblio.org>
2023-01-22 18:12:14 -0500

    fix merge

--- src/nu/xom/tests/BuilderTest.java
+++ src/nu/xom/tests/BuilderTest.java
@@ -427,25 +427,6 @@ public class BuilderTest extends XOMTestCase {
     }
     
     
-    // verify that we use true XML 1.0 name rules; not the retcon
-    // rules from the fifth edition
-    public void testFifthEdition() throws IOException {
-        
-    	// \u1200/ is an Ethiopic character added in Unicode 3.0.
-    	// It is allowed in XML names by the fifth edition but not 
-    	// by earlier editions.
-        String data = "<ro\u1200ot/>";
-        try {
-            builder.build(data, "http://www.example.com");
-            fail("XML Fifth Edition allowed");
-        }
-        catch (ParsingException ex) {
-            assertNotNull(ex.getMessage());   
-        }
-        
-    }
-    
-    
     public void testXML11() throws IOException {
         
         String data = "<?xml version='1.1'?><root/>";
