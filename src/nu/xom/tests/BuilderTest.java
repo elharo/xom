5d02d64a98ca84954a4f42c07576becdafb475d0
Elliotte Rusty Harold <elharo@ibiblio.org>
2026-06-22 02:09:22 -0400

    test XML document that exceeds Java limits

--- src/nu/xom/tests/BuilderTest.java
+++ src/nu/xom/tests/BuilderTest.java
@@ -3029,6 +3029,70 @@ public class BuilderTest extends XOMTestCase {
     }
     
     
+    private static class HugeCommentInputStream extends InputStream {
+        
+        private static final byte[] PREFIX;
+        private static final byte[] SUFFIX;
+        static {
+            try {
+                PREFIX = "<root><!--".getBytes("UTF-8");
+                SUFFIX = "--></root>".getBytes("UTF-8");
+            }
+            catch (UnsupportedEncodingException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        private static final long COMMENT_LENGTH = (long) Integer.MAX_VALUE + 10;
+        
+        private int prefixIndex;
+        private long commentIndex;
+        private int suffixIndex;
+        
+        public int read(byte[] b, int off, int len) {
+            if (prefixIndex < PREFIX.length) {
+                int toCopy = Math.min(len, PREFIX.length - prefixIndex);
+                System.arraycopy(PREFIX, prefixIndex, b, off, toCopy);
+                prefixIndex += toCopy;
+                return toCopy;
+            }
+            if (commentIndex < COMMENT_LENGTH) {
+                int toCopy = (int) Math.min(len, COMMENT_LENGTH - commentIndex);
+                for (int i = 0; i < toCopy; i++) {
+                    b[off + i] = 'A';
+                }
+                commentIndex += toCopy;
+                return toCopy;
+            }
+            if (suffixIndex < SUFFIX.length) {
+                int toCopy = Math.min(len, SUFFIX.length - suffixIndex);
+                System.arraycopy(SUFFIX, suffixIndex, b, off, toCopy);
+                suffixIndex += toCopy;
+                return toCopy;
+            }
+            return -1;
+        }
+        
+        public int read() {
+            byte[] b = new byte[1];
+            int n = read(b, 0, 1);
+            if (n == -1) return -1;
+            return b[0] & 0xFF;
+        }
+        
+    }
+    
+    
+    private static class NullPointerReader extends XMLFilterImpl {
+        
+        public void setFeature(String name, boolean value) {};
+        
+        public void parse(InputSource in) {
+            throw new NullPointerException("test null pointer");
+        }
+        
+    }
+    
+    
     public void testParserThrowsNullPointerException() 
       throws SAXException, IOException, ParsingException {
         checkParserThrows(new NullPointerException());        
@@ -3082,6 +3146,15 @@ public class BuilderTest extends XOMTestCase {
             assertTrue(success.getMessage().contains(ExceptionTester.class.getName()));
         }
 
+    }
+    
+    
+    public void testHugeCommentCausesOutOfMemoryError() 
+      throws ParsingException, IOException {
+        
+        Builder builder = new Builder();
+        builder.build(new HugeCommentInputStream());
+        
     }
     
     
